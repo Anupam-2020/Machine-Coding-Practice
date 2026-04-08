3d7c44ce4c769d18336369703f0c2c758dd5a489
Anupam-2020 <anupamanand.696@gmail.com>
2026-04-08 14:32:08 +0530

    added hover effect

--- accordion/src/App.tsx
+++ accordion/src/App.tsx
@@ -7,25 +7,28 @@ const data = [
 ];
 
 function App() {
-  // const [showItem, setShowItem] = useState<number | null>(null); // this is if we want only 1 item to be open at once.
-  const [showItems, setShowItems] = useState<number[]>([]); // this is if multiple items need to be open when clicked.
+  const [showItem, setShowItem] = useState<number | null>(null); // this is if we want only 1 item to be open at once.
+  // const [showItems, setShowItems] = useState<number[]>([]); // this is if multiple items need to be open when clicked.
 
 
-  const handleClick = (index: number) => {
-    // setShowItem(prev => prev === index ? null : index);
-    setShowItems((prev) =>
-      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
-    );
+  const handleClick = (index: number | null) => {
+    setShowItem(prev => prev === index ? null : index);
+    // setShowItems((prev) =>
+    //   prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
+    // );
   };
 
   return (
     <div className="App">
       {data.map((item) => {
-        const isOpen = showItems.includes(item.id);
+        // const isOpen = showItems.includes(item.id);
+        const isOpen = showItem === item.id;
         return (
           <div
             key={item.id}
-            onClick={() => handleClick(item.id)}
+            // onClick={() => handleClick(item.id)}
+            onMouseEnter={() => handleClick(item.id)}
+            onMouseLeave={() => handleClick(null)}
             style={{ width: "40%", display: "flex", flexDirection: "column" }}
           >
             <div style={{ border: "1px solid white" }}>{item.question}</div>
