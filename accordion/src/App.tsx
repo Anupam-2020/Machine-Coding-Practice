7b05b964b3ae66bea65f618d3414bc7ff191a6ad
Anupam-2020 <anupamanand.696@gmail.com>
2026-04-08 14:23:25 +0530

    added animation to accordion

--- accordion/src/App.tsx
+++ accordion/src/App.tsx
@@ -6,15 +6,16 @@ const data = [
   { id: 3, question: "What is State?", answer: "Component memory" },
 ];
 
-
 function App() {
-
   // const [showItem, setShowItem] = useState<number | null>(null); // this is if we want only 1 item to be open at once.
   const [showItems, setShowItems] = useState<number[]>([]); // this is if multiple items need to be open when clicked.
 
+
   const handleClick = (index: number) => {
     // setShowItem(prev => prev === index ? null : index);
-    setShowItems(prev => prev.includes(index) ? prev.filter(i => i !== index) : [...prev, index]);
+    setShowItems((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
+    );
   };
 
   return (
@@ -27,8 +28,15 @@ function App() {
             onClick={() => handleClick(item.id)}
             style={{ width: "40%", display: "flex", flexDirection: "column" }}
           >
-            <span style={{ border: "1px solid white" }}>{item.question}</span>
-            {isOpen && <span>{item.answer}</span>}
+            <div style={{ border: "1px solid white" }}>{item.question}</div>
+            <div
+              style={{
+                maxHeight: isOpen ? '200px' : '0px',
+                overflow: 'hidden',
+                transition: "all 0.3s ease",
+                padding: isOpen ? '10px' : '0px'
+              }}
+            >{item.answer}</div>
           </div>
         );
       })}
